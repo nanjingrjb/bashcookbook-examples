c69a9504a0933d0ffa94c0695a0767adcb096db9
nanjingrjb <nanjingrjb@gmail.com>
2023-11-15 15:30:01 +0800

    增加背景色函数设置

--- ch07/color_str.sh
+++ ch07/color_str.sh
@@ -24,14 +24,30 @@ set_color()
 		*)echo "30m"
 	esac
 }
+set_back()
+{
 
+	case "$2" in
+		"黑色") echo "40";;
+		"红色") echo "41";;
+		"绿色")echo "42";;
+		"棕色")echo "43";;
+		"蓝色")echo "44";;
+		"洋红")echo "45";;
+		"蓝绿")echo "46";;
+		"白色")echo "47";;
+		*)echo "40"
+	esac
+}
 set_content_color()
 {
 	colstr="$1"
-	cont="$2"
+	colback="$2"
+	cont="$3"
 
 	colFlg=$(set_color $colstr)
-	set_str_cont="\033[$colFlg \"$cont\" \033[0m"
+	backFlg=$(set_back $colback)
+	set_str_cont="\033[$backFlg;$colFlg \"$cont\" \033[0m"
 
 	echo -e "$set_str_cont"
 }
@@ -39,5 +55,5 @@ set_content_color()
 color=("黑色" "红色" "绿色" "棕色" "蓝色" "洋红" "蓝绿" "白色")
 for i in "${color[@]}"
 do
-	set_content_color "$i" "饶家齐 饶剑波 闻婧"
+	set_content_color "$i"  "$i"  "饶家齐 饶剑波 闻婧"
 done
