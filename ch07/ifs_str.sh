64d3e111e1f20ca5f1e1ff81b0c0043f0f8efc40
nanjingrjb <nanjingrjb@gmail.com>
2023-11-24 15:41:00 +0800

    改变IFS 内部字段分割符 Internal Filed Separator 来解析特定的数据
    实际也可以用gawk来解析。

---
+++ ch07/ifs_str.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ***************************************************************************
+# * 
+# * @file:ifs_str.sh 
+# * @author:nanjingrjb@gmail.com 
+# * @date:2023-11-24 15:36 
+# * @version 1.0  
+# * @description: Shell script 
+# * @Copyright (c)  all right reserved 
+#* 
+#**************************************************************************/ 
+
+
+
+        line="root:x:0:0:root:/root:/bin/bash"
+        oldIFS=$IFS;
+        IFS=":"
+        count=0
+        for item in $line;
+        do         
+			[ $count -eq  0 ]    && user=$item;
+            [ $count -eq 6 ]   && shell=$item;
+        let count++
+	    done
+    IFS=$oldIFS
+    echo "$user's shell is $shell"
