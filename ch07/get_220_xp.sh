d0258aa05ae4c493fbd4d565d7e811adcd86efd9
nanjingrjb <nanjingrjb@gmail.com>
2023-11-30 15:42:05 +0800

    echo ".cd 1cd 2cd pcd"|grep -o "[\.a-z]cd"  grep需要对于.$将转义字符\，这与加不加双引号、单引号无关。
    echo "\$cd .cd 1cd xcd"|grep -o '[^\$\.0-9]cd'
    需要对.$进行转义处理。

--- ch07/get_220_xp.sh
+++ ch07/get_220_xp.sh
@@ -10,8 +10,7 @@ get_220_xp()
 
 	icd_path=$1
 	echo "path=$icd_path"
-
-	ls $icd_path|grep V[0-9].[0-9][0-9].*.icd|cut -d- -f5|xargs |\
+   ls $icd_path| grep -E 'V[0-9]\.[0-9][0-9].*\.icd'|cut -d- -f5|xargs |\
 		sed '/[0-9]+/d'|sed -E 's/[ ]+/,/g' >xp.txt
 }
 
